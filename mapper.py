ec613ba27b43eeda02c8263a2a1326f264ca84dc
Kathryne Taylor <katie.taylor0755@gmail.com>
2020-01-22 06:12:38 +0000

    remove stopwords and punctuation

--- mapper.py
+++ mapper.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import sys
+from sklearn.feature_extraction import stop_words
+import string
+
+stops = set(stop_words.ENGLISH_STOP_WORDS)
+all_stops = stops | set(string.punctuation)
+
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in all_stops:
+	        print '%s\t%s' % (word, "1")
